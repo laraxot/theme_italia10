7b904673dbce78f172448d14564dae898f9d9cb6
marco76tv <marco.sottana@gmail.com>
2022-01-11 16:54:30 +0100

    .

--- Resources/views/layouts/partials/scripts.blade.php
+++ Resources/views/layouts/partials/scripts.blade.php
@@ -9,34 +9,34 @@
 	<script src="/themes/foodpicky/js/foodpicky.min.js"></script>
 	================================================== --}}
 <script>
-    var base_url='{{ Theme::asset('/') }}';
-    var lang='{{ app()->getLocale() }}';
-    {{--  var google_maps_api='{{ config('xra.google.maps.api') }}'; --}}
-@if(\Request::has('address'))
-    var address ="{{ \Request::input('address') }}";
-@endif
-@if(\Request::has('lat') && \Request::has('lng'))
-    var lat ="{{ \Request::input('lat') }}";
-    var lng ="{{ \Request::input('lng') }}";
-@endif
+    var base_url = '{{ Theme::asset('/') }}';
+    var lang = '{{ app()->getLocale() }}';
+    {{-- var google_maps_api='{{ config('xra.google.maps.api') }}'; --}}
+    @if (\Request::has('address'))
+        var address ="{{ \Request::input('address') }}";
+    @endif
+    @if (\Request::has('lat') && \Request::has('lng'))
+        var lat ="{{ \Request::input('lat') }}";
+        var lng ="{{ \Request::input('lng') }}";
+    @endif
 </script>
 @stack('scripts_before')
 @php
-	//Theme::add('/theme/pub/js/jquery.min.js',1);
-	Theme::add('/theme/bc/jquery/dist/jquery.min.js',1);
-	//Theme::add('/theme/pub/js/tether.min.js');
-	Theme::add('/theme/pub/js/bootstrap.min.js');
-	Theme::add('/theme/pub/js/foodpicky.js');   
-	/*
-	Theme::add('/theme/pub/js/bootstrap-slider.min.js');
-	Theme::add('/theme/pub/js/jquery.isotope.min.js');
-	Theme::add('/theme/pub/js/headroom.js');
-	Theme::add('/theme/bc/matchHeight/dist/jquery.matchHeight-min.js');
-	//Theme::add('/theme/pub/js/jquery.validate.min.js');
-	*/
-	Theme::add('/theme/pub/js/xot.js');
+//Theme::add('pub_theme::js/jquery.min.js',1);
+Theme::add('/theme/bc/jquery/dist/jquery.min.js', 1);
+//Theme::add('pub_theme::js/tether.min.js');
+Theme::add('pub_theme::js/bootstrap.min.js');
+Theme::add('pub_theme::js/foodpicky.js');
+/*
+ Theme::add('pub_theme::js/bootstrap-slider.min.js');
+ Theme::add('pub_theme::js/jquery.isotope.min.js');
+ Theme::add('pub_theme::js/headroom.js');
+ Theme::add('/theme/bc/matchHeight/dist/jquery.matchHeight-min.js');
+ //Theme::add('pub_theme::js/jquery.validate.min.js');
+ */
+Theme::add('pub_theme::js/xot.js');
 @endphp
 {!! Theme::showScripts(false) !!}
 @stack('scripts')
 
-{{-- dd(Theme::__getStatic('langs')) --}} 
+{{-- dd(Theme::__getStatic('langs')) --}}
