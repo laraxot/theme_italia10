fe1926e5284604cefc22cb72dea678997aeb7033
marco76tv <marco.sottana@gmail.com>
2022-06-09 09:28:22 +0000

    .

--- Resources/js/myvue3.js
+++ Resources/js/myvue3.js
@@ -7,7 +7,11 @@ import { createApp } from 'vue';
 
 //il componente vue va messo qui
 import Map from '../../../../Modules/Geo/Resources/js/components/map/Map2.vue';
+<<<<<<< Updated upstream
 import VueExamples from '../../../../Modules/Geo/Resources/js/components/map/VueExamples.vue';
+=======
+import Davide from '../../../../Modules/Geo/Resources/js/components/map/Davide.vue';
+>>>>>>> Stashed changes
 
 const app = createApp({
     data() {
@@ -19,5 +23,9 @@ const app = createApp({
 
 //e va aggiunto il componente vue anche qui
 app.component('v-map', Map);
+<<<<<<< Updated upstream
 app.component('v-examples', VueExamples);
+=======
+app.component('v-davide', Davide);
+>>>>>>> Stashed changes
 app.mount('#app');
